a2e0f5d27a3bccd2b5804dac8f68dde0ba9b89dd
Excellence komolafe <excellencekomolafe@gmail.com>
2026-04-29 02:32:10 +0100

    fix: apply cargo fmt formatting

--- creator-keys/src/lib.rs
+++ creator-keys/src/lib.rs
@@ -1075,10 +1075,13 @@ mod tests {
         );
     }
 
-    #[test]
+   #[test]
     fn test_normalize_quote_amount_clamps_large_amount() {
         let large = super::fee::MAX_SAFE_AMOUNT + 1;
-        assert_eq!(super::normalize_quote_amount(large), Ok(Some(super::fee::MAX_SAFE_AMOUNT)));
+        assert_eq!(
+            super::normalize_quote_amount(large),
+            Ok(Some(super::fee::MAX_SAFE_AMOUNT))
+        );
     }
 
     #[test]
